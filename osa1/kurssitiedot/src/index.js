48ecb442f72938e3383354ed76f6a3813ae8317f
Melker Narikka <melker.narikka@helsinki.fi>
2019-11-29 20:06:46 +0200

    p1.1, p1.2 done

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (props) => <h1>{props.course}</h1>
+
+const Part = (props) => <p>{props.part} {props.exercises}</p>
+
+const Content = (props) => {
+	return props.parts.map(p => <Part part={p.part} exercises={p.exercises} />)
+}
+
+const Total = (props) => {
+	const num = props.parts.map(p => p.exercises).reduce((a, b) => a + b, 0)
+	return ( <p>Number of exercises {num}</p> )
+}
+
 const App = () => {
 	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
+	const parts = [
+		{part: 'Fundamentals of React', exercises: 10},
+		{part: 'Using props to pass data', exercises: 7},
+		{part: 'State of a component', exercises: 14}
+	]
 
 	return (
 		<div>
-			<h1>{course}</h1>
-			<p>
-				{part1} {exercises1}
-			</p>
-			<p>
-				{part2} {exercises2}
-			</p>
-			<p>
-				{part3} {exercises3}
-			</p>
-			<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+			<Header course={course} />
+			<Content parts={parts} />
+			<Total parts={parts} />
 		</div>
 	)
 }
