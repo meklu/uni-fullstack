aae833479b2954af7608704a3c4fc43b6b958d68
Melker Narikka <melker.narikka@helsinki.fi>
2020-01-08 17:39:13 +0200

    prettify course exercise totals

--- osa1/kurssitiedot/src/components/Total.js
+++ osa1/kurssitiedot/src/components/Total.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Total = (props) => {
 	const num = props.parts.map(p => p.exercises).reduce((a, b) => a + b, 0)
-	return ( <p>Number of exercises {num}</p> )
+	return ( <p><strong>Number of exercises {num}</strong></p> )
 }
 
 export default Total
